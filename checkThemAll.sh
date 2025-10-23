506f90532f391b22042150b3c77f26c4a3ecbb22
Marcelo Cabello <mcabello@cens.cl>
2025-10-23 15:36:28 +0000

    daily update status
    Signed-off-by: Marcelo Cabello <mcabello@cens.cl>

--- checkThemAll.sh
+++ checkThemAll.sh
@@ -12,10 +12,10 @@ bash checkServer.sh http://lacpass-dev.salud.gob.sv:8080/snowstorm/fhir http://n
 bash checkServer.sh http://fhir.mspas.gob.gt:8180 http://fhir.mspas.org/terminology A-11 GUATEMALA & \
 bash checkServer.sh http://181.210.30.59:8180/fhir http://node-acme.org/terminology A02BC0100 HONDURAS & \
 bash checkServer.sh http://190.34.154.93:8180/fhir http://racsel.org/antecedentes E03.9 PANAMA & \
-bash checkServer.sh https://snowstorm.mspbs.gov.py/fhir http://node-acme.org/terminology 33 PARAGUAY & \ 
+bash checkServer.sh https://snowstorm.mspbs.gov.py/fhir http://node-acme.org/terminology 33 PARAGUAY & \
 bash checkServer.sh https://dyakuter.minsa.gob.pe/fhir http://node-PE.org/terminology 90633.01 PERU & \
 bash checkServer.sh http://154.38.173.158:8180/fhir http://node-x.org/terminology C910 REPDOM & \
-bash checkServer.sh http://186.179.201.48:8080/fhir http://node-acme.org/terminology 0 SURINAME & \
-bash checkServer.sh http://179.27.170.27:8180/fhir http://node-UY.org/terminology 10 URUGUAY
+bash checkServer.sh http://186.179.201.48:8180/fhir http://node-acme.org/terminology R81 SURINAME & \
+bash checkServer.sh http://179.27.170.27:8180/fhir http://node-UY.org/terminology 10 URUGUAY && \
 
 echo "Finished"
